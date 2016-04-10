673bfbd70f13703e509de72c709bac68a3c8b8f7
shamwell <jared.shamwell@gmail.com>
2016-04-10 16:18:45 -0400

    proj2

---
+++ update_ws_proj_2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~/ros_ws/src
+wstool merge https://raw.githubusercontent.com/cmsc421/cmsc421/master/cmsc421.rosinstall
+wstool update
+cd ~/ros_ws
+catkin_make
+#cp -r ~/ros_ws/src/baxter_htn/models/* ~/.gazebo/models
+source ~/.bashrc
+sleep 5
+rospack profile
